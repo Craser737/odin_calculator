3e5890040a71eedc909c7285373f9bb6b6de6d2e
Craser <101993553+Craser737@users.noreply.github.com>
2024-02-29 13:16:03 -0800

    Add color and shading. Start javaScript assigns

---
+++ javaScript.js
@@ -0,0 +1,56 @@
+const ac = document.querySelector('#ac');
+const one = document.querySelector('#1');
+const two = document.querySelector('#2');
+const three = document.querySelector('#3');
+const four = document.querySelector('#4');
+const five = document.querySelector('#5');
+const six = document.querySelector('#6');
+const seven = document.querySelector('#7');
+const eight = document.querySelector('#8');
+const nine = document.querySelector('#9');
+const zero = document.querySelector('#0');
+const period = document.querySelector('#.');
+const del = document.querySelector('#del');
+const multiply = document.querySelector('#*');
+const divide = document.querySelector('#/');
+const plus = document.querySelector('#+');
+const minus = document.querySelector('#-');
+const plusMinus = document.querySelector('#+/-');
+const percent = document.querySelector('#%');
+const enter = document.querySelector('#=');
+const display = document.querySelector('#display');
+const calc = document.querySelector('#calc');
+const number = '';
+calc.addEventListener('click', (e) => {
+    const event = e.target;
+
+    switch (event) {
+        case ac:        
+
+        case one:
+        case two:
+        case three:
+        case four:
+        case five:
+        case six:
+        case seven:
+        case eight:
+        case nine:
+        case zero:
+        case period:
+        case del:
+        case multiply:
+        case divide:
+        case plus:
+        case minus:
+        case plusMinus:
+        case percent:
+        case enter:
+    }
+});
+
+
+
+
+
+
