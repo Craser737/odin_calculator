79651d65cdfef808dc7c7358d6a059dd0c1bcc60
Craser <101993553+Craser737@users.noreply.github.com>
2024-03-01 09:43:38 -0800

    complete JavaScript functionality

--- javaScript.js
+++ javaScript.js
@@ -1,56 +1,164 @@
 const ac = document.querySelector('#ac');
-const one = document.querySelector('#1');
-const two = document.querySelector('#2');
-const three = document.querySelector('#3');
-const four = document.querySelector('#4');
-const five = document.querySelector('#5');
-const six = document.querySelector('#6');
-const seven = document.querySelector('#7');
-const eight = document.querySelector('#8');
-const nine = document.querySelector('#9');
-const zero = document.querySelector('#0');
-const period = document.querySelector('#.');
+const one = document.querySelector('#one');
+const two = document.querySelector('#two');
+const three = document.querySelector('#three');
+const four = document.querySelector('#four');
+const five = document.querySelector('#five');
+const six = document.querySelector('#six');
+const seven = document.querySelector('#seven');
+const eight = document.querySelector('#eight');
+const nine = document.querySelector('#nine');
+const zero = document.querySelector('#zero');
+const decimal = document.querySelector('#decimal');
 const del = document.querySelector('#del');
-const multiply = document.querySelector('#*');
-const divide = document.querySelector('#/');
-const plus = document.querySelector('#+');
-const minus = document.querySelector('#-');
-const plusMinus = document.querySelector('#+/-');
-const percent = document.querySelector('#%');
-const enter = document.querySelector('#=');
+const multiplyBtn = document.querySelector('#multiply');
+const divideBtn = document.querySelector('#divide');
+const plus = document.querySelector('#plus');
+const minus = document.querySelector('#minus');
+const plusMinus = document.querySelector('#plusMinus');
+const percent = document.querySelector('#percent');
+const equal = document.querySelector('#enter');
 const display = document.querySelector('#display');
 const calc = document.querySelector('#calc');
-const number = '';
-calc.addEventListener('click', (e) => {
-    const event = e.target;
+let number = '';
+let numArray = [];
+let operator = '';
+let screenClear = false;
 
-    switch (event) {
-        case ac:        
-
-        case one:
-        case two:
-        case three:
-        case four:
-        case five:
-        case six:
-        case seven:
-        case eight:
-        case nine:
-        case zero:
-        case period:
-        case del:
-        case multiply:
-        case divide:
-        case plus:
-        case minus:
-        case plusMinus:
-        case percent:
-        case enter:
-    }
+document.addEventListener('click', (e) => {
+    const event = e.target.textContent;
+    eventSwitch(event);
+    e.target.blur();
+});
+document.addEventListener('keydown', (e) => {
+    const keyEvent = e.key;
+    eventSwitch(keyEvent);
 });
 
+function eventSwitch(event) {
+    switch (event) {
+        case 'AC':        
+            clear();
+            break;
+        case 'Delete':
+            clear();
+            break;
+        case '1':
+            numBtn(1)
+            break;
+        case '2':
+            numBtn(2)
+            break;
+        case '3':
+            numBtn(3)
+            break;
+        case '4':
+            numBtn(4);
+            break;
+        case '5':
+            numBtn(5);5
+            break;
+        case '6':
+            numBtn(6);
+            break;
+        case '7':
+            numBtn(7);
+            break;
+        case '8':
+            numBtn(8);
+            break;
+        case '9':
+            numBtn(9);
+            break;
+        case '0':
+            numBtn(0);
+            break;
+        case '.':
+            numBtn('.');
+            break;
+        case 'del':
+            number = number.slice(0, -1);
+            break;
+        case 'Backspace':
+            number = number.slice(0, -1);
+            break;
+        case '*':
+            operation('*');
+            break;
+        case 'x':
+            operation('*');
+            break;
+        case '/':
+            operation('/');
+            break;
+        case '+':
+            operation('+');
+            break;
+        case '-':
+            operation('-');
+            break;
+        case '+/-':
+            if (number.charAt(0) !== '-') {
+                number = '-' + number;
+            } else {
+                number = number.slice(1);
+            }
+            break;
+        case '%':
+            number = (number / 100).toString();
+            break;
+        case '=':1
+            enter();
+            break;
+        case 'Enter':
+            enter();
+            break;
+    }    
+    
+    if (event ==='Delete' || event === 'AC') {
+        display.textContent = '0';
+    } else {
+        display.textContent = number;
+    }
+};
 
+function clear() {
+    number = '';
+    numArray = [];
+    operator = '';
+}
 
+function numBtn(num) {
+    if (screenClear) {
+        number = '';
+        screenClear = false;
+    }
+    number = number.concat(num);
+}
 
+function operation(oper) {
+    numArray.push(number);
+    screenClear = true;
+    operator = oper;
+}
 
-
+function enter() {
+    numArray.push(number);
+    switch (operator) {
+        case '+':
+            number = numArray.reduce((a, b) => Number(a) + Number(b)).toString();
+            break;
+        case '-':
+            number = numArray.reduce((a, b) => Number(a) - Number(b)).toString();
+            break;
+        case '*':
+            number = numArray.reduce((a, b) => Number(a) * Number(b)).toString();
+            break;
+        case '/':
+            number = numArray.reduce((a, b) => Number(a) / Number(b)).toString();
+            break;    
+        };
+    
+    numArray = [];
+    screenClear = true;
+}
